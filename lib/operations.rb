f972a264c8542d8b0cff6f7cc4b5a533154d51eb
Milly Flores <miflo0h@gmail.com>
2020-05-14 18:38:28 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,7 @@
 
 
 def unsafe?(speed)
-  if  speed > 60 speed < 40 
+  if  speed > 60 || speed < 40 
     true 
   else
     false
@@ -11,7 +11,7 @@ end
 
 
 def not_safe?(speed)
-	speed > 60 speed < 40
+	speed > 60 || speed < 40 ?  true : false
 end
 	
 
