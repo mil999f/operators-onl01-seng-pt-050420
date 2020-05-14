178699677960ca7b2a70a17d07369db154e2a2ce
Milly Flores <miflo0h@gmail.com>
2020-05-14 18:03:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,6 @@
-speed = 60
-unsafe = ''; 
-speed > 40 ? (speed ='40!'; puts "Safe!")
-  : (speed = '39!'; puts "unsafe!");
 
-
-
-
-def unsafe?(speed = 60 ; puts "Safe!")
+def unsafe?(speed = 60 : puts "Safe!")
+  
 
 end
 
